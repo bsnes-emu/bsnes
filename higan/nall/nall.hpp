94b2538af5849f7e194b35c0e823a266eee182ea
Tim Allen <screwtape@froup.com>
2012-08-16 20:30:47 +1000

    Update to higan v091 release.
    byuu says:
    
    Basically just a project rename, with s/bsnes/higan and the new icon
    from lowkee added in.
    
    It won't compile on Windows because I forgot to update the resource.rc
    file, and a path transform command isn't working on Windows.
    It was really just meant as a starting point, so that v091 WIPs can flow
    starting from .00 with the new name (it overshadows bsnes v091, so
    publicly speaking this "shouldn't exist" and will probably be deleted
    from Google Code when v092 is ready.)

---
+++ higan/nall/nall.hpp
@@ -0,0 +1,57 @@
+#ifndef NALL_HPP
+#define NALL_HPP
+
+//include the most common nall headers with one statement
+//does not include the most obscure components with high cost and low usage
+
+#include <nall/platform.hpp>
+
+#include <nall/algorithm.hpp>
+#include <nall/any.hpp>
+#include <nall/atoi.hpp>
+#include <nall/base64.hpp>
+#include <nall/bit.hpp>
+#include <nall/bmp.hpp>
+#include <nall/config.hpp>
+#include <nall/crc32.hpp>
+#include <nall/directory.hpp>
+#include <nall/dl.hpp>
+#include <nall/endian.hpp>
+#include <nall/file.hpp>
+#include <nall/filemap.hpp>
+#include <nall/function.hpp>
+#include <nall/gzip.hpp>
+#include <nall/http.hpp>
+#include <nall/image.hpp>
+#include <nall/inflate.hpp>
+#include <nall/interpolation.hpp>
+#include <nall/intrinsics.hpp>
+#include <nall/invoke.hpp>
+#include <nall/map.hpp>
+#include <nall/png.hpp>
+#include <nall/property.hpp>
+#include <nall/random.hpp>
+#include <nall/serializer.hpp>
+#include <nall/set.hpp>
+#include <nall/sha256.hpp>
+#include <nall/sort.hpp>
+#include <nall/stdint.hpp>
+#include <nall/stream.hpp>
+#include <nall/string.hpp>
+#include <nall/traits.hpp>
+#include <nall/unzip.hpp>
+#include <nall/utility.hpp>
+#include <nall/varint.hpp>
+#include <nall/vector.hpp>
+#include <nall/zip.hpp>
+
+#if defined(PLATFORM_WINDOWS)
+  #include <nall/windows/registry.hpp>
+  #include <nall/windows/utf8.hpp>
+#endif
+
+#if defined(PLATFORM_X)
+  #include <nall/serial.hpp>
+#endif
+
+#endif
---
+++ higan/target-ethos/bootstrap.cpp
@@ -0,0 +1,17 @@
+#include <fc/interface/interface.hpp>
+#include <sfc/interface/interface.hpp>
+#include <gb/interface/interface.hpp>
+#include <gba/interface/interface.hpp>
+//#include <nds/interface/interface.hpp>
+
+void Application::bootstrap() {
+  interface = new Interface;
+
+  emulator.append(new Famicom::Interface);
+  emulator.append(new SuperFamicom::Interface);
+  emulator.append(new GameBoy::Interface);
+  emulator.append(new GameBoyAdvance::Interface);
+//emulator.append(new NintendoDS::Interface);
+
+  for(auto &system : emulator) system->bind = interface;
+}
