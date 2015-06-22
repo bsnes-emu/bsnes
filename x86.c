6b44980c6c00e6b429613402e55ffb8a95e391d6
Tim Allen <screwtape@froup.com>
2015-06-22 23:31:49 +1000

    Update to v094r29 release.
    byuu says:
    
    Note: for Windows users, please go to nall/intrinsics.hpp line 60 and
    correct the typo from "DISPLAY_WINDOW" to "DISPLAY_WINDOWS" before
    compiling, otherwise things won't work at all.
    
    This will be a really major WIP for the core SNES emulation, so please
    test as thoroughly as possible.
    
    I rewrote the 65816 CPU core's dispatcher from a jump table to a switch
    table. This was so that I could pass class variables as parameters to
    opcodes without crazy theatrics.
    
    With that, I killed the regs.r[N] stuff, the flag_t operator|=, &=, ^=
    stuff, and all of the template versions of opcodes.
    
    I also removed some stupid pointless flag tests in xcn and pflag that
    would always be true.
    
    I sure hope that AWJ is happy with this; because this change was so that
    my flag assignments and branch tests won't need to build regs.P into
    a full 8-bit variable anymore.
    
    It does of course incur a slight performance hit when you pass in
    variables by-value to functions, but it should help with binary size
    (and thus cache) by reducing a lot of extra functions. (I know I could
    have used template parameters for some things even with a switch table,
    but chose not to for the aforementioned reasons.)
    
    Overall, it's about a ~1% speedup from the previous build. The CPU core
    instructions were never a bottleneck, but I did want to fix the P flag
    building stuff because that really was a dumb mistake v_v'

--- x86.c
+++ x86.c
@@ -6,6 +6,7 @@
 
 #define LIBCO_C
 #include "libco.h"
+
 #include <assert.h>
 #include <stdlib.h>
 
@@ -13,10 +14,10 @@
 extern "C" {
 #endif
 
-#if defined(_MSC_VER)
-  #define fastcall __fastcall
-#elif defined(__GNUC__)
+#if defined(__clang__) || defined(__GNUC__)
   #define fastcall __attribute__((fastcall))
+#elif defined(_MSC_VER)
+  #define fastcall __fastcall
 #else
   #error "libco: please define fastcall macro"
 #endif
@@ -25,10 +26,20 @@ static thread_local long co_active_buffer[64];
 static thread_local cothread_t co_active_handle = 0;
 static void (fastcall *co_swap)(cothread_t, cothread_t) = 0;
 
-//ABI: fastcall
+/* ABI: fastcall */
 static unsigned char co_swap_function[] = {
-  0x89, 0x22, 0x8B, 0x21, 0x58, 0x89, 0x6A, 0x04, 0x89, 0x72, 0x08, 0x89, 0x7A, 0x0C, 0x89, 0x5A,
-  0x10, 0x8B, 0x69, 0x04, 0x8B, 0x71, 0x08, 0x8B, 0x79, 0x0C, 0x8B, 0x59, 0x10, 0xFF, 0xE0,
+  0x89, 0x22,        /* mov [edx],esp    */
+  0x8b, 0x21,        /* mov esp,[ecx]    */
+  0x58,              /* pop eax          */
+  0x89, 0x6a, 0x04,  /* mov [edx+ 4],ebp */
+  0x89, 0x72, 0x08,  /* mov [edx+ 8],esi */
+  0x89, 0x7a, 0x0c,  /* mov [edx+12],edi */
+  0x89, 0x5a, 0x10,  /* mov [edx+16],ebx */
+  0x8b, 0x69, 0x04,  /* mov ebp,[ecx+ 4] */
+  0x8b, 0x71, 0x08,  /* mov esi,[ecx+ 8] */
+  0x8b, 0x79, 0x0c,  /* mov edi,[ecx+12] */
+  0x8b, 0x59, 0x10,  /* mov ebx,[ecx+16] */
+  0xff, 0xe0,        /* jmp eax          */
 };
 
 #ifdef _WIN32
@@ -51,7 +62,7 @@ static unsigned char co_swap_function[] = {
 #endif
 
 static void crash() {
-  assert(0); /* called only if cothread_t entrypoint returns */
+  assert(0);  /* called only if cothread_t entrypoint returns */
 }
 
 cothread_t co_active() {
@@ -66,14 +77,14 @@ cothread_t co_create(unsigned int size, void (*entrypoint)(void)) {
     co_swap = (void (fastcall*)(cothread_t, cothread_t))co_swap_function;
   }
   if(!co_active_handle) co_active_handle = &co_active_buffer;
-  size += 256; /* allocate additional space for storage */
-  size &= ~15; /* align stack to 16-byte boundary */
+  size += 256;  /* allocate additional space for storage */
+  size &= ~15;  /* align stack to 16-byte boundary */
 
   if(handle = (cothread_t)malloc(size)) {
-    long *p = (long*)((char*)handle + size); /* seek to top of stack */
-    *--p = (long)crash;                      /* crash if entrypoint returns */
-    *--p = (long)entrypoint;                 /* start of function */
-    *(long*)handle = (long)p;                /* stack pointer */
+    long *p = (long*)((char*)handle + size);  /* seek to top of stack */
+    *--p = (long)crash;                       /* crash if entrypoint returns */
+    *--p = (long)entrypoint;                  /* start of function */
+    *(long*)handle = (long)p;                 /* stack pointer */
   }
 
   return handle;
