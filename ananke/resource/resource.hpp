b113ecb5a3b69b9a2152c993f40dc2360493a48b
Tim Allen <screwtape@froup.com>
2015-10-08 22:02:22 +1100

    Deleting ananke and shaders.
    ananke has been superseded by icarus.
    
    The new tomoko UI does not support shaders, and if it ever does it will
    probably use another format, so not much point keeping the old files
    around.

--- ananke/resource/resource.hpp
+++
@@ -1,7 +0,0 @@
-namespace resource {
-  extern const uint8_t home[606];
-  extern const uint8_t up[652];
-  extern const uint8_t folder[1176];
-  extern const uint8_t file[844];
-  extern const uint8_t archive[1067];
-};
