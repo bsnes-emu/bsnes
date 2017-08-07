accffa8d1bb8e85e906c39057c71f6a26e68bee2
Tim Allen <screwtape@froup.com>
2017-08-07 22:12:24 +1000

    A tool to check our docs for broken links.
    Annoyingly, mkdocs won't validate inter- or intra-document links,
    so we'll need to use a third-party tool instead.

---
+++ docs/checklinks.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# This uses the official W3C link-checker tool:
+#
+#   https://github.com/w3c/link-checker
+#
+checklink \
+	--summary \
+	--broken \
+	--location=http://127.0.0.1:8000/ \
+	--exclude 'github.com|board.byuu.org' \
+	http://127.0.0.1:8000/
