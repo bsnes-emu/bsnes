a04646ab5b0962551dedf24f3c6860f41c6ce160
radius <andres.430@gmail.com>
2018-02-03 21:14:31 -0500

    update Android.mk

--- libretro/jni/Android.mk
+++ libretro/jni/Android.mk
@@ -27,9 +27,17 @@ CORE_DIR := ../..
 
 include ../Makefile.common
 
+$(CORE_DIR)/libretro/%_boot.c: $(CORE_DIR)/BootROMs/prebuilt/%_boot.bin
+        echo "/* AUTO-GENERATED */" > $@
+        echo "const unsigned char $(notdir $(@:%.c=%))[] = {" >> $@
+        hexdump -v -e '/1 "0x%02x, "' $< >> $@
+        echo "};" >> $@
+        echo "const unsigned $(notdir $(@:%.c=%))_length = sizeof($(notdir $(@:%.c=%)));" >> $@
+
 LOCAL_SRC_FILES := $(SOURCES_CXX) $(SOURCES_C)
-LOCAL_CXXFLAGS += -DINLINE=inline -DHAVE_STDINT_H -DHAVE_INTTYPES_H -D__LIBRETRO__ -DNDEBUG -D_USE_MATH_DEFINES  -DGB_INTERNAL
+LOCAL_CFLAGS += -DINLINE=inline -DHAVE_STDINT_H -DHAVE_INTTYPES_H -D__LIBRETRO__ -DNDEBUG -D_USE_MATH_DEFINES  -DGB_INTERNAL
 
 LOCAL_C_INCLUDES = $(INCFLAGS)
 
 include $(BUILD_SHARED_LIBRARY)
+
