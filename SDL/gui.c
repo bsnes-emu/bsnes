3a4ed6fd40b0e53e3345f605c72f0f0c226cdf25
Lior Halphon <LIJI32@gmail.com>
2018-06-23 22:36:29 +0300

    Made SDL menus more consistent between keyboards and joypads (Closes #35)

--- SDL/gui.c
+++ SDL/gui.c
@@ -773,7 +773,7 @@ void run_gui(bool is_running)
                     if (button == JOYPAD_BUTTON_A) {
                         event.key.keysym.scancode = SDL_SCANCODE_RETURN;
                     }
-                    else if (button == JOYPAD_BUTTON_START || button == JOYPAD_BUTTON_B || button == JOYPAD_BUTTON_MENU) {
+                    else if (button == JOYPAD_BUTTON_MENU) {
                         event.key.keysym.scancode = SDL_SCANCODE_ESCAPE;
                     }
                     else if (button == JOYPAD_BUTTON_UP) event.key.keysym.scancode = SDL_SCANCODE_UP;
