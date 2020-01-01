892f202945879f644e5535a383d241db1c7c3bdc
byuu <2107894+byuu@users.noreply.github.com>
2020-01-01 18:45:11 +0900

    Updated SNES game databases

--- bsnes/target-bsnes/resource/resource.hpp
+++ bsnes/target-bsnes/resource/resource.hpp
@@ -3,7 +3,7 @@ extern const unsigned char Icon[3463];
 extern const unsigned char Logo[23467];
 extern const unsigned char SameBoy[32358];
 namespace System {
-extern const unsigned char Boards[31515];
+extern const unsigned char Boards[31506];
 extern const unsigned char IPLROM[64];
 }
 }
