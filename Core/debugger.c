e35c22d4059a70c2d79409adc0b3224d121b66bf
Lior Halphon <LIJI32@gmail.com>
2020-09-17 23:47:35 +0300

    Fix a potential single byte overflow

--- Core/debugger.c
+++ Core/debugger.c
@@ -132,7 +132,7 @@ static const char *value_to_string(GB_gameboy_t *gb, uint16_t value, bool prefer
     }
 
     /* Avoid overflow */
-    if (symbol && strlen(symbol->name) > 240) {
+    if (symbol && strlen(symbol->name) >= 240) {
         symbol = NULL;
     }
 
@@ -172,7 +172,7 @@ static const char *debugger_value_to_string(GB_gameboy_t *gb, value_t value, boo
     }
 
     /* Avoid overflow */
-    if (symbol && strlen(symbol->name) > 240) {
+    if (symbol && strlen(symbol->name) >= 240) {
         symbol = NULL;
     }
 
