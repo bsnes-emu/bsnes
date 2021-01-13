6dca01ad274e46383a4a97b4b0c1eabfcd83a7d6
Jeff Verkoeyen <jverkoey@gmail.com>
2021-01-13 13:59:28 -0500

    Annotate properties as nonatomic.

--- Cocoa/AppDelegate.h
+++ Cocoa/AppDelegate.h
@@ -2,15 +2,15 @@
 
 @interface AppDelegate : NSObject <NSApplicationDelegate, NSUserNotificationCenterDelegate, NSMenuDelegate>
 
-@property IBOutlet NSWindow *preferencesWindow;
-@property (strong) IBOutlet NSView *graphicsTab;
-@property (strong) IBOutlet NSView *emulationTab;
-@property (strong) IBOutlet NSView *audioTab;
-@property (strong) IBOutlet NSView *controlsTab;
+@property (nonatomic, strong) IBOutlet NSWindow *preferencesWindow;
+@property (nonatomic, strong) IBOutlet NSView *graphicsTab;
+@property (nonatomic, strong) IBOutlet NSView *emulationTab;
+@property (nonatomic, strong) IBOutlet NSView *audioTab;
+@property (nonatomic, strong) IBOutlet NSView *controlsTab;
 - (IBAction)showPreferences: (id) sender;
 - (IBAction)toggleDeveloperMode:(id)sender;
 - (IBAction)switchPreferencesTab:(id)sender;
-@property (weak) IBOutlet NSMenuItem *linkCableMenuItem;
+@property (nonatomic, weak) IBOutlet NSMenuItem *linkCableMenuItem;
 
 @end
 
