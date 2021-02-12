9975ede3ef0967e4a93caa5951033b2082310f7b
ds22x <45218067+ds22x@users.noreply.github.com>
2021-02-13 00:52:22 +0100

    Hide Bottom Most Scanline When Playing SGB Games
    Depending on the game, the bottom most scanline, when playing SGB, is either completely black, or flashes when the screen updates.
    This hides the line when the game currently used is a GameBoy game, and only when "Show Overscan Area" is disabled.

--- bsnes/target-bsnes/program/platform.cpp
+++ bsnes/target-bsnes/program/platform.cpp
@@ -216,7 +216,14 @@ auto Program::videoFrame(const uint16* data, uint pitch, uint width, uint height
   if(!settings.video.overscan) {
     uint multiplier = height / 240;
     data += 8 * multiplier * pitch;
-    height -= 16 * multiplier;
+	if (gameBoy.program)
+		{
+			height -= 16.1 * multiplier;
+		}
+		else
+		{
+			height -= 16 * multiplier;
+		}
   }
 
   uint outputWidth, outputHeight;
