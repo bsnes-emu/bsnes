bf2ab3f751942e2c3346c2b8908ff4aff3159e5d
Morilli <35152647+Morilli@users.noreply.github.com>
2021-07-28 06:11:32 +0200

    Re-add WINVER and _WIN32_WINNT defines as apparently older mingw-w64 versions didn't define them high enough

--- nall/windows/guard.hpp
+++ nall/windows/guard.hpp
@@ -5,12 +5,16 @@
 #define interface WindowsInterface
 
 #undef UNICODE
+#undef WINVER
 #undef WIN32_LEAN_AND_MEAN
+#undef _WIN32_WINNT
 #undef NOMINMAX
 #undef PATH_MAX
 
 #define UNICODE
+#define WINVER 0x0601
 #define WIN32_LEAN_AND_MEAN
+#define _WIN32_WINNT WINVER
 #define NOMINMAX
 #define PATH_MAX 260
 
