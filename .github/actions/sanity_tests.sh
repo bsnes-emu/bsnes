859ff79527beb83c01ad3dbdd8865885b48d98ea
Lior Halphon <LIJI32@gmail.com>
2022-07-25 23:10:30 +0300

    Fix sanity test

--- .github/actions/sanity_tests.sh
+++ .github/actions/sanity_tests.sh
@@ -16,10 +16,10 @@ set +e
 
 FAILED_TESTS=`
 shasum .github/actions/*.bmp | grep -E -v \(\
-5283564df0cf5bb78a7a90aff026c1a4692fd39e\ \ .github/actions/cgb-acid2.bmp\|\
+7673e1836eaa28c2967689d0b399f69a597c0c5a\ \ .github/actions/cgb-acid2.bmp\|\
 dbcc438dcea13b5d1b80c5cd06bda2592cc5d9e0\ \ .github/actions/cgb_sound.bmp\|\
 0caadf9634e40247ae9c15ff71992e8f77bbf89e\ \ .github/actions/dmg-acid2.bmp\|\
-a732077f98f43d9231453b1764d9f797a836924d\ \ .github/actions/dmg-mode-acid2.bmp\|\
+e3494dcba47af69e8d35fb84c49c272a0d735809\ \ .github/actions/dmg-mode-acid2.bmp\|\
 c9e944b7e01078bdeba1819bc2fa9372b111f52d\ \ .github/actions/dmg_sound-2.bmp\|\
 f0172cc91867d3343fbd113a2bb98100074be0de\ \ .github/actions/oam_bug-2.bmp\
 \)`
