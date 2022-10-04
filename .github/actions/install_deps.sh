51510b53fd95b8170a2e94811cd8d904f2fdd7e8
Lior Halphon <LIJI32@gmail.com>
2022-10-04 14:59:01 +0300

    Update GitHub Actions to use RGBDS 0.6.0

--- .github/actions/install_deps.sh
+++ .github/actions/install_deps.sh
@@ -4,7 +4,7 @@ case `echo $1 | cut -d '-' -f 1` in
                 sudo apt-get install -yq bison libpng-dev pkg-config libsdl2-dev
                 (
                 cd `mktemp -d`
-                curl -L https://github.com/rednex/rgbds/archive/v0.4.0.zip > rgbds.zip
+                curl -L https://github.com/rednex/rgbds/archive/v0.6.0.zip > rgbds.zip
                 unzip rgbds.zip
                 cd rgbds-*
                 make -sj
