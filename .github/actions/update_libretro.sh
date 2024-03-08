06bc4cd0bfa05bef1ef193e7881aa7786d7cc0b7
Lior Halphon <LIJI32@gmail.com>
2024-03-08 18:16:51 +0200

    Make the libretro branch only use stable releases, tag libretro releases

---
+++ .github/actions/update_libretro.sh
@@ -0,0 +1,25 @@
+set -e
+
+LATEST=$(git tag --sort=-creatordate | grep "^v" | grep -v libretro | head -n 1)
+
+if [ $(git tag -l "$LATEST"-libretro) ]; then
+    echo "The libretro branch is already up-to-date"
+    exit 0
+fi
+
+echo "Building boot ROMs..."
+make -j bootroms
+mv build/bin/BootROMs BootROMs/prebuilt
+
+echo "Updating branch"
+git config --global --add --bool push.autoSetupRemote true
+git config --global user.name 'Libretro Updater'
+git config --global user.email '<>'
+
+git branch --delete libretro || true
+git checkout tags/$LATEST -b libretro
+
+git add BootROMs/prebuilt/*
+git commit -m "Update libretro branch to $LATEST"
+git tag "$LATEST"-libretro
+git push --force --tags
