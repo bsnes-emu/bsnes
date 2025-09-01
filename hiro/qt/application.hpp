40b1ff1491e72ec5fe603d1a1e02eaee1db09af3
John Chadwick <john@jchw.io>
2025-08-31 21:31:56 -0400

    hiro/qt: Remove pendingEvents, add timeout to processEvents
    Qt 6 no longer allows us to poll for the status of the event loop.
    However, Qt 6 does still have a processEvents function, and the version
    with the timeout actually spins the event loop and returns as early as
    there are no more events in it. That's basically what we want to do. We
    could run this with a really huge timeout, but that would just make it
    more likely that hiro freezes up, so instead let's mirror hiro/gtk3 and
    use a 50ms timeout.

--- hiro/qt/application.hpp
+++ hiro/qt/application.hpp
@@ -6,7 +6,6 @@ struct pApplication {
   static auto exit() -> void;
   static auto modal() -> bool;
   static auto run() -> void;
-  static auto pendingEvents() -> bool;
   static auto processEvents() -> void;
   static auto quit() -> void;
   static auto setScreenSaver(bool screenSaver) -> void;
